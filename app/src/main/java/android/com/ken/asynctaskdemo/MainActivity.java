63f74ecc80f5a9721ff840a82489be5261330ce9
ketan1705 <ketanagrawal459@gmail.com>
2023-08-11 13:40:46 +0530

    Commit

---
+++ app/src/main/java/android/com/ken/asynctaskdemo/MainActivity.java
@@ -0,0 +1,46 @@
+package android.com.ken.asynctaskdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+
+        new DownloadImage().execute("https://picsum.photos/200/300?grayscale");
+    }
+
+    private class DownloadImage extends AsyncTask<String,Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            String url = strings[0];
+            Bitmap bitmap = null;
+            try {
+                InputStream inputStream = new java.net.URL(url).openStream();
+                bitmap = BitmapFactory.decodeStream(inputStream);
+            }catch (Exception exception)
+            {
+                exception.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            imageView.setImageBitmap(bitmap);
+        }
+    }
+}
